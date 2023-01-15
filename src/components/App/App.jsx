38b7ede3075dee3fbda452c2a137d5a6e0f0b00f
Lesnik <airvaffeforce@yandex.ru>
2023-01-15 12:04:09 +0300

    active/finished tasks counters added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,21 @@
+import React, {useState} from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
-
-
+import mockData from '../../mock-data/mock-data';
 
 import './App.css';
 
 function App() {
+  const [boardState, setBoardState] = useState(mockData)
   return (
     <div className='wrapper'>
       <Header title="Awesome Kanban Board" />
-      <Main />
-      <Footer />
-      
+      <Main boardState={boardState} setBoardState={setBoardState} />
+      <Footer 
+      activeTasks={boardState[0].issues.length} 
+      finishedTask={boardState[3].issues.length}
+      />
     </div>
   );
 }
