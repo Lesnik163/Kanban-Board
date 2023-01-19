53c598806dab6cb59782b84d7aceb2fb4afcdf46
Lesnik <airvaffeforce@yandex.ru>
2023-01-20 00:14:45 +0300

    routes was corrected

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -10,18 +10,18 @@ const Avatar = () => {
     return(
         <div className='avatar'>
             <img onClick={toggleDropDownList} 
-            src="../Kanban-Board/logo/user-avatar.svg"  
+            src="../logo/user-avatar.svg"  
             className='avatar-img' alt="User" />
             <img onClick={toggleDropDownList} 
             className='arrow' 
-            src="../Kanban-Board/logo/arrow-down.svg" 
+            src="../logo/arrow-down.svg" 
             alt="arrow-down" hidden={!dropDownHidden}/>
             <img onClick={toggleDropDownList} 
-            src="../Kanban-Board/logo/arrow-up.svg" 
+            src="../logo/arrow-up.svg" 
             alt="arrow-up" 
             className='arrow' hidden={dropDownHidden}/>
             <img  
-            src="../Kanban-Board/logo/Diamond.svg" 
+            src="../logo/Diamond.svg" 
             alt="Diamond" hidden={dropDownHidden}/>
             <ul className='avatar-list' hidden={dropDownHidden}>
                 <li ><a href='#'>{'Profile'}</a></li>
